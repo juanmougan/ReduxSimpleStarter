4b88330370552d9c9e4916bae1f90e78ac44f84a
Juan Manuel Mougan <juanmougan@gmail.com>
2017-03-31 22:45:57 -0300

    Added class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 // Functional component: literally, a function
-// Class component, instead, has internal record keeping
-const SearchBar = () => {
-	return <input />;
+// Class component
+class SearchBar extends React.Component {
+  // every class based component must have a render method
+  // It's a function
+  render() {
+  	// must return some JSX
+  	return <input />;
+  }
 }
+
 
 export default SearchBar;
