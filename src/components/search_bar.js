21a85921eff8a385b73cadb64af94bc0d3ba9d4c
Juan Manuel Mougan <juanmougan@gmail.com>
2017-03-31 22:47:57 -0300

    JSX sintactic sugar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
+// this is syntactic sugar for this (equivalent)
+// const Component = React.Component
 
 // Functional component: literally, a function
 // Class component
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   // every class based component must have a render method
   // It's a function
   render() {
