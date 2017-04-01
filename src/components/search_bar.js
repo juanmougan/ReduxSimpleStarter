b62167367a0aeaca6ef03ad0e9c82be5ef7bc1d6
Juan Manuel Mougan <juanmougan@gmail.com>
2017-03-31 22:40:05 -0300

    Comment

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// Functional component: literally, a function
+// Class component, instead, has internal record keeping
 const SearchBar = () => {
 	return <input />;
 }
