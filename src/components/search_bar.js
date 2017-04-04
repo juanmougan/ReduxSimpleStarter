9042b27cf4383e9ae964380a956a7ae8e65f6462
Juan Manuel Mougan <juanmougan@gmail.com>
2017-04-03 23:13:49 -0300

    Started with state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,6 +5,16 @@ import React, { Component } from 'react';
 // Functional component: literally, a function
 // Class component
 class SearchBar extends Component {
+  
+  // Each time state changes, Component will be re-rendered
+  
+  // Initialize state inside the class' constructor
+  // First function called when obj is instantiated
+  constructor(props) {
+  	super(props);
+  	this.state = { term: '' };
+  }
+
   // every class based component must have a render method
   // It's a function
   render() {
